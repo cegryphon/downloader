fe60a384b1629da244c601bf1c71f17c52c02d3d
cegryphon <cale@thegriffins.org>
2013-10-27 14:39:02 -0500

    FIXED THE LOOP

--- src/org/thegriffins/cale/downloader/Downloader.java
+++ src/org/thegriffins/cale/downloader/Downloader.java
@@ -22,22 +22,26 @@ public class Downloader {
         int slash = args[0].lastIndexOf('/');
         String name = args[0].substring(slash+1);
         System.out.println("Name: " + name);
+        FileOutputStream file = new FileOutputStream(name);
         
-        //URL url = new URL(args[0]);
-        
-        //InputStream stream = url.openStream();
-    
         URL url = new URL (args[0]);
         InputStream stream = url.openStream();
-        byte[] bytes=new byte[10000000];
-        int got = stream.read(bytes);
-        stream.close();
-        System.out.println("Size: " + got);
+        byte[] bytes=new byte[10000];
+        int size = 0;
         
-        //I am commenting
-        FileOutputStream file = new FileOutputStream(name);
-        file.write(bytes,0,got); 
-        //I am commenting again
+        while (true) {
+            int got = stream.read(bytes);
+            if (got<0) {
+                break;
+            }
+
+            size = size + got;
+            file.write(bytes,0,got); 
+        }
+        
+        System.out.println("Size: " + size);        
+        stream.close();        
         file.close();
+        
     }
 }
